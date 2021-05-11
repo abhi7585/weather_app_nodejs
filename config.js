4e655a710dedff58ef07c892362c029e4c73dc13
Abhishek Tripathi <54246710+abhi7585@users.noreply.github.com>
2021-05-11 20:18:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
     openWeatherMap: {
         BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY: "68e8410ea5bda4695134f9f86d2f8959"
+        SECRET_KEY: "Your API key which you will get from OPEN WEATHER API"
     }
 }
 
